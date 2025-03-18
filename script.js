4347b55649d8fbeeca525d654c5b3b2e8b4b479b
LarisaFrescher75 <stas.frescher@gmail.com>
2025-03-18 21:30:40 +0100

    add commit

--- script.js
+++ script.js
@@ -11,4 +11,32 @@ flag = true;
 var arr = [1, 2, 3, 4, 5];
 // arr.push('5') Пример ошибки типизации
 var arr1 = ["1", "2", "3"];
-var arr2 = ['1', '2', '3', '4'];
+var arr2 = [1, '2', 3, '4'];
+console.log("Hello");
+// Переменные разных типов
+//let productName = "Laptop"; // Название товара
+var productName = "Laptop";
+//let price = 1499.99; // Цена товара
+var price = 1499.99;
+//let inStock = true; // Есть ли товар в наличии
+var inStock = true;
+inStock = false;
+//let discount = null; // Скидка пока не установлена
+var discount = null;
+//let warranty; // Переменная без значения (undefined)
+var warranty;
+// Переменная, которая может быть числом или строкой
+var productCode = 1010;
+//productCode = "A1010";
+// Массив чисел (цены товаров)
+var prices = [499, 1299, 799, 2499, 1599];
+// prices.push("2000"); // Ошибка должна быть в TS
+// Массив строк (названия товаров)
+var products = ["Phone", "Tablet", "Monitor", "Keyboard", "Mouse"];
+// Массив, содержащий только числа или только строки
+var ids1 = ["ID001", "ID002", "ID003"]; // Только строки
+var ids2 = [101, 102, 103]; // Только числа
+// Массив с числами и строками одновременно
+var mixedValues = [1, "two", 3, "four", 5, "six"];
+// Массив булевых значений
+var toggles = [true, false, false, true, true];
--- script.ts
+++ script.ts
@@ -27,4 +27,53 @@ const arr2: (number  | string)[] = [ 1, '2', 3, '4'];
 console.log("Hello");
 
 
+// Переменные разных типов
+//let productName = "Laptop"; // Название товара
+
+let productName : string = "Laptop";
+
+//let price = 1499.99; // Цена товара
+
+let price : number = 1499.99;
+
+//let inStock = true; // Есть ли товар в наличии
+
+let inStock : boolean = true;
+inStock = false;
+
+
+//let discount = null; // Скидка пока не установлена
+
+let discount : null = null;
+
+//let warranty; // Переменная без значения (undefined)
+let warranty : any;
+
+
+// Переменная, которая может быть числом или строкой
+let productCode : number | string = 1010;
+//productCode = "A1010";
+
+// Массив чисел (цены товаров)
+const prices : number[] = [499, 1299, 799, 2499, 1599];
+// prices.push("2000"); // Ошибка должна быть в TS
+
+
+
+// Массив строк (названия товаров)
+const products : string[] = ["Phone", "Tablet", "Monitor", "Keyboard", "Mouse"];
+
+
+// Массив, содержащий только числа или только строки
+const ids1 : string[] = ["ID001", "ID002", "ID003"]; // Только строки
+
+
+const ids2 : number[] = [101, 102, 103]; // Только числа
+
+// Массив с числами и строками одновременно
+const mixedValues : (string | number)[]= [1, "two", 3, "four", 5, "six"];
+
+// Массив булевых значений
+const toggles : boolean[]= [true, false, false, true, true];
+
 
